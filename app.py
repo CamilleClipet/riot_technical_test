bebdc4461ba859c46930d0fc9d85a37c64ed70d7
Camille Clipet <camilleclipet@mac.lan>
2025-03-20 10:42:47 +0100

    implement sign and verify endpoints + remove extra line

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, Response, request
+import json
 
 from business_logic.decryption import decrypt_json_depth_1
 from business_logic.encryption import encrypt_json_depth_1
+from business_logic.signing import compute_hmac_signature
+from business_logic.verifying import verify_signature
 
 app = Flask(__name__)
 
@@ -34,11 +37,44 @@ def _decrypt() -> Response:
         return Response(str(e), status=500)
 
 
-@app.route("/sign")
+@app.route("/sign", methods=["POST"])
 def _sign():
-    return "to be implemented"
+    raw_data = request.data
+    if raw_data is None:
+        return Response("Bad Request", status=400)
+
+    try:
+        json_data = raw_data.decode("utf-8")
+        # remove whitespaces and line breaks
+        json_data = json.dumps(json.loads(json_data)).strip()
+        result = compute_hmac_signature(json_data)
+        return Response(
+            json.dumps({"signature": result}), status=200, mimetype="application/json"
+        )
+
+    except Exception as e:
+        return Response(str(e), status=500)
 
 
-@app.route("/verify")
+@app.route("/verify", methods=["POST"])
 def _verify():
-    return "to be implemented"
+    data = request.json
+    if data is None:
+        return Response("Bad Request", status=400)
+    try:
+        signature = data.get("signature")
+        payload = data.get("data")
+        if (
+            signature
+            and payload
+            and isinstance(signature, str)
+            and isinstance(payload, dict)
+        ):
+            is_verified = verify_signature(
+                signature=signature, payload=json.dumps(payload)
+            )
+            if is_verified:
+                return Response("Signature Verified", status=204)
+        return Response("Signature Verification Failed", status=400)
+    except Exception as e:
+        return Response(str(e), status=500)
--- business_logic/signing.py
+++ business_logic/signing.py
@@ -10,7 +10,6 @@ load_dotenv()
 def compute_hmac_signature(payload: str) -> str:
     if not isinstance(payload, str):
         raise ValueError("Input must be a valid JSON string")
-
     payload_bytes = payload.encode("utf-8")
     secret_key_bytes = os.getenv("SECRET_KEY").encode("utf-8")
 
